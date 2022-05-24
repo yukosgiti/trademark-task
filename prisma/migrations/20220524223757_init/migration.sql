352ea979c508800d835c07cd7341f9256c63adad
Yüksel Kapan <yukselkapan@gmail.com>
2022-05-25 01:38:10 +0300

    Fix relation error in scheama.

---
+++ prisma/migrations/20220524223757_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `goodsServiceId` on the `ClassDescription` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ClassDescription" DROP COLUMN "goodsServiceId";
