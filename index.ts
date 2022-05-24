4f3530d973154086e573aa983a8043725062dfa6
Yüksel Kapan <yukselkapan@gmail.com>
2022-05-25 00:47:52 +0300

    Add .env checks.

--- index.ts
+++ index.ts
@@ -1,12 +1,36 @@
 import { PrismaClient } from '@prisma/client'
-import { convertXMLToTradeMarkJson } from './scripts'
+import { convertXMLToTradeMarkJson, populateDatabase } from './scripts'
+import 'dotenv/config'
+import path from 'path';
+import fs from "fs/promises"
 
 const prisma = new PrismaClient()
 
-async function main() {
+async function ensureEnv() {
 
-  await convertXMLToTradeMarkJson()
-  // ... you will write your Prisma Client queries here
+  if (process.env.FILES_PATH == null) {
+    throw Error("Please define 'FILE_PATH' environment variable.")
+  }
+  let filesPathStat = await fs.lstat(process.env.FILES_PATH);
+
+  if (!filesPathStat.isDirectory()) {
+    throw Error("'FILE_PATH' is not a directory.")
+  }
+
+  if (process.env.SAVE_PATH == null) {
+    throw Error("Please define 'SAVE_PATH' environment variable.")
+  }
+
+  let savePathStat = await fs.lstat(process.env.SAVE_PATH);
+  if (!savePathStat.isDirectory()) {
+    throw Error("'SAVE_PATH' is not a directory.")
+  }
+}
+
+async function main() {
+  await ensureEnv();
+  await populateDatabase(prisma)
+  // await convertXMLToTradeMarkJson()
 }
 
 main()
