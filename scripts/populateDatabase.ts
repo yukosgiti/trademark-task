4376c273fa75dbc1733fe845d1e86ada96b6dad3
Yüksel Kapan <yukselkapan@gmail.com>
2022-05-25 02:08:37 +0300

    Update populate database script.

--- scripts/populateDatabase.ts
+++ scripts/populateDatabase.ts
@@ -7,7 +7,53 @@ async function populateDatabase(client: PrismaClient) {
   for (const path of trademarkPaths) {
     let tradeMarkJSON = await parseJSONFile(`${process.env.SAVE_PATH}/${path}`)
     let tradeMarkObj = new TradeMark(tradeMarkJSON)
-    console.log(tradeMarkJSON)
+    console.log({ file: `${process.env.SAVE_PATH}/${path}`, tradeMarkJSON })
+
+    let result = await client.tradeMark.create({
+      data: {
+        applicationDate: tradeMarkObj.applicationDate,
+        applicationNumber: tradeMarkObj.applicationNumber!,
+        markCurrentStatusCode: tradeMarkObj.markCurrentStatusCode,
+        operationCode: tradeMarkObj.operationCode!,
+        wordMark: tradeMarkObj.wordMark,
+        applicationLanguageCode: tradeMarkObj.applicationLanguageCode,
+        secondLanguageCode: tradeMarkObj.secondLanguageCode,
+        expiryDate: tradeMarkObj.expiryDate,
+        kindMark: tradeMarkObj.kindMark,
+        tradeDistinctivenessIndicator: tradeMarkObj.tradeDistinctivenessIndicator,
+        markCurrentStatusCodeMilestone: tradeMarkObj.markCurrentStatusCodeMilestone,
+        markCurrentStatusCodeStatus: tradeMarkObj.markCurrentStatusCodeStatus,
+        Publication: {
+          createMany: {
+            data: tradeMarkObj.publications?.map(i => ({
+              publicationDate: i.publicationDate!,
+              publicationIdentifier: i.publicationIdentifier!,
+              publicationPage: i.publicationPage,
+              publicationSection: i.publicationSection!
+            })) ?? []
+          }
+        },
+        ClassDescription: {
+          createMany: {
+            data: tradeMarkObj.classDescriptions?.map(i => ({
+              classNumber: i.classNumber!
+            })) ?? []
+          }
+        }
+      }
+    })
+
+    tradeMarkObj.classDescriptions?.forEach(async (i) => {
+      await client.classDescription.create({
+        data: {
+          classNumber: i.classNumber!,
+          tradeMarkId: result.id,
+          GoodsServiceDescription: {
+            create: i.descriptions?.map(j => ({ description: j.description, languageCode: j.languageCode }))
+          }
+        }
+      })
+    })
   }
 }
 
